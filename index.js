7521d1fb753e792d594dac2021d5557973b85d99
Frank Arias <32846100+frankliworld@users.noreply.github.com>
2023-07-26 19:14:20 -0500

    fix improved features with async/await

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 const path = require('node:path')
 
+const NO_FILE = 'ENOENT'
 const LIST_ERRORS = {
   NO_PATH: 'No se ha especificado el path del archivo',
-  NO_WORD: 'No se ha especificado la palabra a buscar'
+  NO_WORD: 'No se ha especificado la palabra a buscar',
 }
+
 // Ejercicio 2
-async function writeFile (filePath, data, callback) {
-  const dirPath = path.dirname(filePath);
-  fs.mkdirSync(dirPath, { recursive: true });
-  fs.writeFile(filePath, data, callback);
+async function writeFile(filePath, data, callback) {
+  try {
+    const dirPath = path.dirname(filePath)
+    await fs.mkdir(dirPath, {recursive: true});
+    await fs.writeFile(filePath, data);
+    callback(null);
+  } catch (err) {
+    callback(err)
+  }
 }
+
 // Ejercicio 3
 async function readFileAndCount(word, callback) {
   const argv = process.argv[2];
-  if (!argv) return callback(new Error(LIST_ERRORS.NO_PATH)); // r: No se ha especificado el path del archivo
-  if (!word) return callback(new Error(LIST_ERRORS.NO_WORD)); // r: No se ha especificado la palabra a buscar
-  fs.exists(argv, (existFilePath) => {
-    if (!existFilePath) return callback(null, 0); // r: El archivo no existe
-    fs.readFile(argv, 'utf8', (err, data) => {
-      if (err) return callback(err);
-      const count = (data.match(new RegExp(word, 'g')) ?? []).length; // Otra forma de hacerlo: data.split(word).length - 1;
-      callback(null, count);
-    });
-  });
+
+  if (!argv) {
+    const err = new Error(LIST_ERRORS.NO_PATH);
+    return callback(err);
+  }
+  if (!word) {
+    const err = new Error(LIST_ERRORS.NO_WORD);
+    return callback(err);
+  }
+
+  try {
+    await fs.access(argv);
+    const data = await fs.readFile(argv, 'utf8');
+    const searchWord = data.match(new RegExp(word, 'g')) ?? [];
+    const count = searchWord.length;
+    callback(null, count);
+  } catch (err) {
+    if (err.code === NO_FILE) {
+      callback(null, 0); // El archivo no existe
+    } else {
+      callback(err);
+    }
+  }
 }
+
 
 module.exports = {
   writeFile,
